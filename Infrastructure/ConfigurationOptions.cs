36d4ab470a0313d07cb5aedf11386d2b0acdc0bd
grish <grishka@duck.com>
2025-04-27 06:59:13 +0300

    add property file

--- Infrastructure/ConfigurationOptions.cs
+++ Infrastructure/ConfigurationOptions.cs
@@ -15,4 +15,14 @@ namespace HandMidiControllerDDD.Infrastructure
         public int DeviceId { get; set; } = 0;
         public int MidiChannel { get; set; } = 0;
     }
+    public class RabbitMqOptions
+    {
+        public string Hostname { get; set; } = "localhost";
+        public int Port { get; set; } = 5672;
+        public string VirtualHost { get; set; } = "/";
+        public string Username { get; set; } = "guest";
+        public string Password { get; set; } = "guest";
+        public string ExchangeName { get; set; } = "midi.events";
+        public bool AlsoSendToLocalDevice { get; set; } = true;
+    }
 }
